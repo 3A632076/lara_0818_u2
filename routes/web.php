cb88ad75fd775808b653bb4bbf2064993b91c172
Lin Ming Ying <moon9638150@gmail.com>
2019-10-29 18:52:26 +0800

    修改剛剛沒新增到的指令

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('home',function(){
-    return view('home');
-});
-Route::get('home',function(){
-    return view('home.index');
-});
+//Route::get('home',function(){
+//    return view('home');
+//});
+//Route::get('home',function(){
+//    return view('home.index');
+//});
+Route::get('home', "HomeController@index");
