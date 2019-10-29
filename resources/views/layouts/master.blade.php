0d746d3927d4dadccab3659f33c88e63609bbc2f
Lin Ming Ying <moon9638150@gmail.com>
2019-10-29 18:55:49 +0800

    編輯 master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,1 +1,11 @@
-<?php
+
+<html>
+    <head>
+        <title>@yield('title') | My Blog</title>
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
